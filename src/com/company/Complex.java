9af076d8789ad6167c6b3f0e46d5a54372c584a0
student <123>
2022-01-04 12:04:22 -0500

    123

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -55,7 +55,7 @@ public class Complex {
         return c;
     }
 
-
+//TODO: Add more imaginary numbers to test//
 
     public static void main(String[] args) {
         Complex c1= new Complex(3,4);
--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class SoccerTeam {
+    private int win;
+    private int lose;
+    private int tie;
+
+    public SoccerTeam(){
+        win=0;
+        lose=0;
+        tie=0;
+    }
+    public SoccerTeam
 }
